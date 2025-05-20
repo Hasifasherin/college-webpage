69c52f313ac4faacac4997822dcdd0e42d5a965a
Hasifa sherin CP <hasifasherincp@gmail.com>
2025-05-20 23:04:52 +0530

    Day-24 image slider

--- script.js
+++ script.js
@@ -112,3 +112,24 @@ courses.forEach(function (course) {
   li.textContent = course;
   courseList.appendChild(li);
 });
+
+ let currentIndex = 0;
+    const slides = document.getElementById('slideContainer');
+    const totalSlides = slides.children.length;
+
+    function showSlide(index) {
+      slides.style.transform = `translateX(-${index * 100}%)`;
+    }
+
+    function nextSlide() {
+      currentIndex = (currentIndex + 1) % totalSlides;
+      showSlide(currentIndex);
+    }
+
+    function prevSlide() {
+      currentIndex = (currentIndex - 1 + totalSlides) % totalSlides;
+      showSlide(currentIndex);
+    }
+
+    // Auto slide every 6 seconds
+    setInterval(nextSlide, 6000);
